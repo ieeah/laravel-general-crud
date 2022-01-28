317aa0c64f4987aaf7f4f8c7b2c1aafcbabb1009
ieeah <lbnfnc90@gmail.com>
2022-01-28 16:19:59 +0100

    project setup and home page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+@extends('layout.main')
+
+@section('content')
+	<div class="home_page">
+		<div class="hero">
+			
+			<div class="hero_title">
+				<h1> Come prendi la temperatura di una capra?</h1>
+				<h3>Molto Attentamente!</h3>
+			</div>
+		</div>
+		<div class="container content">
+			<p>
+				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit, molestias! Id earum dolorem quas nesciunt, perspiciatis saepe nam asperiores totam nisi similique error odit animi facere, veniam inventore. Quaerat quam suscipit repellat ad sit laboriosam cum? Officia harum dolore modi doloremque, aliquid dolorem incidunt culpa nulla libero voluptates laborum? Harum eveniet repellendus odio placeat aut quae. Fugiat unde nemo quisquam quidem voluptates consequatur corrupti iusto perferendis aliquam aut sequi voluptatum, mollitia enim animi cumque ex voluptas consequuntur quam perspiciatis asperiores qui saepe officia? Dolores, minus fuga voluptatem doloribus temporibus ipsam nisi eaque? Dolorum blanditiis vero voluptatum, doloremque, rem harum nemo fuga quis aliquam aliquid, perferendis recusandae? Vel veritatis numquam ipsa similique, perspiciatis enim vero minus? Voluptatum minus omnis explicabo quia minima eos aliquid nisi fugiat? Doloremque aspernatur quos ullam earum id voluptates, doloribus non eos dolor vitae ad maxime, iure unde mollitia architecto optio dolorum. Enim veritatis fugiat recusandae soluta, quis blanditiis eos ratione beatae! Error placeat pariatur deserunt, excepturi neque cum corrupti dignissimos inventore, voluptate tempora vitae commodi delectus. Animi sunt, minima consequatur asperiores ipsa excepturi aliquid voluptates ad tempore assumenda. Sint perspiciatis ipsa placeat est, laboriosam corrupti autem velit assumenda rerum. Repudiandae quae a dicta quos qui! Velit minus commodi quasi asperiores reprehenderit! Sint unde odio nisi aliquid illum voluptas cupiditate praesentium sapiente earum nobis. Aut, velit quaerat! Ea praesentium velit pariatur consectetur harum numquam, architecto enim eius quos suscipit at porro nesciunt, dicta, blanditiis sit quas doloribus corrupti quisquam! Error architecto quam accusantium vitae officia excepturi vel sequi id hic deserunt repudiandae ab totam quae eveniet harum voluptatem, atque commodi omnis doloremque. Facilis neque nemo, veritatis a, expedita inventore dolor labore soluta maxime quis quidem accusamus! Iste odio repudiandae natus voluptas, hic nisi vero similique cum, delectus aliquid praesentium velit minus sed consectetur, reprehenderit ipsa ipsam mollitia!
+			</p>
+		</div>
+	</div>
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,18 @@
+<header class="py-3 bg-warning">
+	<div class="container d-flex justify-content-between">
+		<div class="brand">
+			<a class="text-uppercase text-decoration-none text-black fw-bold" href="">
+				<img src="{{asset('imgs/Caprette soloLogo.svg')}}" alt="">
+				<span>Caprette joke: le barzellette delle caprette</span>
+			</a>
+		</div>
+		<nav>
+			<ul class="d-flex align-content-center">
+				<li class="list-unstyled ms-3"><a class="fw-bold fs-5 text-black text-decoration-none" href="">Archivio</a></li>
+				<li class="list-unstyled ms-3"><a class="fw-bold fs-5 text-black text-decoration-none" href="">Crea</a></li>
+				<li class="list-unstyled ms-3"><a class="fw-bold fs-5 text-black text-decoration-none" href="">Random</a></li>
+			</ul>
+		</nav>
+		
+	</div>
+</header>
